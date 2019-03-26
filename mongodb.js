d47aaef105180fa530ae83271ae0468aa06a5e37
danny <danixeka@gmail.com>
2019-03-26 19:56:30 +1100

    was getting error - cannot find module - because i did not run npm init and npm install

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,14 @@
 // CRUD create read update destroy
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 
 // Destructured the above
-const { MongoClient, ObjectID } = require('mongodb')
+// const { MongoClient, ObjectID } = require('mongodb')
+
+const id = new ObjectID()
+console.log(id)
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
