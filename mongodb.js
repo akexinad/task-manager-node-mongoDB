13f1906f9a7306c0b88d17ec21184f16b040f4f3
danny <danixeka@gmail.com>
2019-03-28 15:59:33 +1100

    deleting tasks

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,5 @@
 // CRUD create read update destroy
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
 
-// Destructured the above
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -18,27 +14,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  // db.collection('users').updateOne({
-  //   _id: new ObjectID("5c99effce4f198d45895f8d6")
-  // }, {
-  //   $inc: {
-  //     age: 100
-  //   }
-  // }).then( (result) => {
-  //   console.log(result);
-  // }).catch( (error) => {
-  //   console.log(error);
-  // })
-
-  db.collection('tasks').updateMany({
-    completed: false
-  }, {
-    $set: {
-      completed: true
-    }
-  }).then( (result) => {
-    console.log(result.modifiedCount);
-  }).catch( (error) => {
+  db.collection('tasks').deleteOne({
+    description: "Mop floor"
+  }).then( result => {
+    console.log(result.deletedCount);
+  }).catch( error => {
     console.log(error);
   })
 
