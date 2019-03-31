cb017d1bf5150fa57a19c715a2bc86fe486df994
danny <danixeka@gmail.com>
2019-04-01 09:20:52 +1100

    changing chained promises to async await

--- misc/3-promise-chaining.js
+++ misc/3-promise-chaining.js
@@ -1,14 +1,28 @@
 require('../src/db/mongoose.js')
 const User = require('../src/models/user.js')
 
-User.findByIdAndUpdate('5ca04f961c94d15a81d309b7', { age: 1 })
-.then( (user) => {
-  console.log(user);
-  return User.countDocuments({ age: 1 })
+// User.findByIdAndUpdate('5ca04f961c94d15a81d309b7', { age: 1 })
+// .then( (user) => {
+//   console.log(user);
+//   return User.countDocuments({ age: 1 })
+// })
+// .then( (result) => {
+//   console.log(result);
+// })
+// .catch( (e) => {
+//   console.log(e);
+// })
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
+  return count
+}
+
+updateAgeAndCount('5ca04f961c94d15a81d309b7', 2)
+.then( count => {
+  console.log(count);
 })
-.then( (result) => {
-  console.log(result);
-})
-.catch( (e) => {
+.catch( e => {
   console.log(e);
 })
--- misc/4-promise-chaining-2.js
+++ misc/4-promise-chaining-2.js
@@ -1,14 +1,28 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task.js')
 
-Task.findByIdAndDelete({ _id: '5ca06523203448611378da6e' })
-.then( (task) => {
-  console.log(task);
-  return Task.countDocuments({ completed: false })
+// Task.findByIdAndDelete({ _id: '5ca06523203448611378da6e' })
+// .then( (task) => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false })
+// })
+// .then( (result) => {
+//   console.log(result);
+// })
+// .catch( (e) => {
+//   console.log(e);
+// })
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id)
+  const count = await Task.countDocuments({ completed: false })
+  return count
+}
+
+deleteTaskAndCount('5ca06202069671607a21cc7c')
+.then( count => {
+  console.log(count);
 })
-.then( (result) => {
-  console.log(result);
-})
-.catch( (e) => {
+.catch( e => {
   console.log(e);
 })
