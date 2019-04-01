d1b80e45402d9a2d148ee742d395297e61651edf
danny <danixeka@gmail.com>
2019-04-01 14:14:15 +1100

    successfully sending delete requests

--- src/index.js
+++ src/index.js
@@ -72,6 +72,22 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req,res) => {
+
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if (!user) {
+      return res.status(404).send({ Error: "User Id Invalid" })
+    }
+
+    res.status(200).send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
@@ -133,6 +149,22 @@ app.patch('/tasks/:id', async (req, res) => {
   } catch (e) {
     res.status(500).send(e)
   }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+
+    if (!task) {
+      return res.status(404).send({ Error: "Task id invalid!"})
+    }
+
+    res.status(200).send(task)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+
 })
 
 app.listen(port, () => {
