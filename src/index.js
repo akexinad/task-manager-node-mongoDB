46f9a8fe2468052833d94c25f3dc566fb41dc789
danny <danixeka@gmail.com>
2019-04-02 09:17:43 +1100

    express middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,24 @@ const taskRouter = require('./routers/task.js')
 const app = express()
 const port = process.env.PORT || 3001
 
+//
+// Without middleware: new request -> run route handler
+//
+// With middleware:    new request -> do something -> run route handler
+//
+
+// app.use( (req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
+app.use( (req, res, next) => {
+  res.status(503).send('Site is under maintenance')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
