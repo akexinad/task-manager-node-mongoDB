849e4c74cb4746c66c6a9c9d0b1d9b952f63ad3b
danny <danixeka@gmail.com>
2019-04-01 13:13:51 +1100

    removed chained promises

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 3001
 
 app.use(express.json())
 
-// ASYNC AWAIT ALWAYS RETURNS A PROMISE
 app.post('/users', async (req, res) => {
   const user = new User(req.body)
 
@@ -19,13 +18,6 @@ app.post('/users', async (req, res) => {
     res.status(400).send(e)
   }
 
-  // user.save()
-  // .then( () => {
-  //   res.status(201).send(user)
-  // })
-  // .catch( (e) => {
-  //   res.status(400).send(e)
-  // })
 })
 
 app.get('/users', async (req, res) => {
@@ -37,13 +29,6 @@ app.get('/users', async (req, res) => {
     res.status(404).send(e)
   }
 
-  // User.find({})
-  // .then( (users) => {
-  //   res.send(users)
-  // })
-  // .catch( (e) => {
-  //   res.status(404).send()
-  // })
 })
 
 app.get('/users/:id', async (req, res) => {
@@ -62,20 +47,6 @@ app.get('/users/:id', async (req, res) => {
     res.status(500).send(e)
   }
 
-  // const _id = req.params.id
-  //
-  // User.findById(_id)
-  // .then( (user) => {
-  //   if (!user) {
-  //     return res.status(404).send()
-  //   }
-  //
-  //   res.send(user)
-  // })
-  // .catch( (e) => {
-  //   res.status(500).send()
-  //
-  // })
 })
 
 app.post('/tasks', async (req, res) => {
@@ -88,13 +59,6 @@ app.post('/tasks', async (req, res) => {
     res.status(400).send(e)
   }
 
-  // task.save()
-  // .then( () => {
-  //   res.status(201).send(task)
-  // })
-  // .catch( (e) => {
-  //   res.status(400).send(e)
-  // })
 })
 
 app.get('/tasks', async (req, res) => {
@@ -106,13 +70,6 @@ app.get('/tasks', async (req, res) => {
     res.status(500).send(e)
   }
 
-  // Task.find({})
-  // .then( (tasks) => {
-  //   res.status(200).send(tasks)
-  // })
-  // .catch( (e) => {
-  //   rest.status(500).send()
-  // })
 })
 
 app.get('/tasks/:id', async (req, res) => {
@@ -130,17 +87,6 @@ app.get('/tasks/:id', async (req, res) => {
     res.status(500).send(e)
   }
 
-  // Task.findById(_id)
-  // .then( (task) => {
-  //   if (!task) {
-  //     return res.status(404).send()
-  //   }
-  //
-  //   res.send(task)
-  // })
-  // .catch( (e) => {
-  //   res.status(500).send()
-  // })
 })
 
 app.listen(port, () => {
