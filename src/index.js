a2bc7ed47d85ab732a3b0beec084d4e07e1f232a
danny <danixeka@gmail.com>
2019-04-01 16:25:27 +1100

    using bcrypt

--- src/index.js
+++ src/index.js
@@ -13,3 +13,20 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log(`Server is up on http://localhost:${ port }`);
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+  const password = 'Red1234567!'
+  const hash = await bcrypt.hash(password, 8)
+
+
+  console.log(password);
+  console.log(hash);
+
+  const isMatch = await bcrypt.compare(password, hash)
+  console.log(isMatch);
+
+}
+
+myFunction()
