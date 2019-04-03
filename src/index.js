c54c4160a0a256be1ecd37de33e70852d635ab6b
danny <danixeka@gmail.com>
2019-04-03 16:18:57 +1100

    validating file uploads with multer and regex

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ const port = process.env.PORT || 3001
 
 const multer = require('multer')
 const upload = multer({
-  dest: 'images' // dest = destination
+  dest: 'images', // dest = destination
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    // These are the three ways you can callback
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cb(undefined, false)
+
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('File must be a Word Document'))
+    }
+
+    cb(undefined, true)
+  }
 })
 
 app.post('/upload', upload.single('upload'), (req, res) => {
