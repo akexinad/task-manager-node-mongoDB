cbcc7f0a69443d2b2f22c708f09c8475586862a6
danny <danixeka@gmail.com>
2019-04-03 11:20:32 +1100

    deleting tasks belonging to the user who deletes his own profile

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task.js')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -112,6 +113,13 @@ userSchema.pre('save', async function (next) {
 
   }
 
+  next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+  const user = this
+  await Task.deleteMany({ owner: user._id })
   next()
 })
 
