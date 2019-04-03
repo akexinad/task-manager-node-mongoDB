807cff5ad78a81520568db7e9cbe73a62d7e9522
danny <danixeka@gmail.com>
2019-04-04 08:54:59 +1100

    integrating sendgrid in my create and delete user routes

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey = 'SG.7zD7Ya7HT7-eMYFx0dLaow.R5nFz3Z_qMem8qozVcRqFEuvVE8I7m
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-  to: 'danny.ceccattini@gmail.com',
-  from: 'danixeka@gmail.com',
-  subject: 'My first sendgrid email!',
-  text: 'Shadows and dust Maximus. Shadows and dust!!!'
-})
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'danixeka@gmail.com',
+    subject: 'Cheers for joining!',
+    text: `Welcome to the app, ${ name }. Let me know how you get along with the app.`
+  })
+}
+
+const sendCancellationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'danixeka@gmail.com',
+    subject: 'Sorry to see you go',
+    text: `Hey ${ name },\n\n We just wanted to say it's a shame that you're going. If you have any feedback on the reasons for your cancellation please let us know, we are always looking to improve the app. Hope to see you again soon!`
+  })
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancellationEmail // ES6 shorthand
+}
