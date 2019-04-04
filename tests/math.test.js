753264c8800d077ebf80102196b1826be9784032
danny <danixeka@gmail.com>
2019-04-05 09:12:11 +1100

    adding more tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,11 +1,21 @@
-const { calculateTip } = require('../src/math.js')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math.js')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
-    expect(total).toBe(13)
+    expect(total).toBe(13) 
 })
 
 test('Should calculate total with default tip', () => {
     const total = calculateTip(10)
     expect(total).toBe(12.5)
+})
+
+test('Should convert 32 F to 0 C', () => {
+    const degCelcius = fahrenheitToCelsius(32)
+    expect(degCelcius).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    const degFarenheit = celsiusToFahrenheit(0)
+    expect(degFarenheit).toBe(32)
 })
