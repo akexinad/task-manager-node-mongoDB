47300e106485372488f1cd92016941c7a8218660
danny <danixeka@gmail.com>
2019-04-05 14:03:07 +1100

    all tests passing so far for login and sign up

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app.js')
+const User = require('../src/models/user.js')
+
+const userOne = {
+    name: "Benigni",
+    email: 'benigni@example.com',
+    password: '44helloworld!'
+}
+
+beforeEach( async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users')
@@ -9,4 +21,23 @@ test('Should sign up a new user', async () => {
         "password": "ottoemezzo1990"
     })
     .expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login')
+    .send({
+        email: userOne.email,
+        password: userOne.password
+    })
+    .expect(200)
+})
+
+test('Should NOT login nonexistant user', async () => {
+    await request(app).post('/users/login')
+    .send({
+        name: userOne.name,
+        email: userOne.email,
+        password: 'password'
+    })
+    .expect(400)
 })
