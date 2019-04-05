8e96a86f674a37ebd82e02b8b8f5ab70d96f4960
danny <danixeka@gmail.com>
2019-04-05 11:02:34 +1100

    testing promises and asynchronous functions

--- src/math.js
+++ src/math.js
@@ -8,8 +8,21 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32
 }
 
+const add = (a, b) => {
+    return new Promise( (resolve, reject) => {
+      setTimeout( () => {
+        if (a < 0 || b < 0) {
+          return reject('Numbers must be > 0')
+        }
+        resolve(a + b)
+      }, 500)
+    })
+  }
+  
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 }
