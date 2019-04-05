4a686231163619db783e8cdbf648e9ac0e12a1ae
danny <danixeka@gmail.com>
2019-04-05 11:02:34 +1100

    testing promises and asynchronous functions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math.js')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math.js')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -18,4 +18,24 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
     const degFarenheit = celsiusToFahrenheit(0)
     expect(degFarenheit).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout( () => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 1000)
+// })
+
+test('Should add two numbers', (done) => {
+    add(2, 3)
+    .then( (sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add two numbers async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
